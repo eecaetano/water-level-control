4bf544cad05aa459f00c8b1a6c6dc2641995df7e
ANDERSON R OLIVEIRA <103950004+eecaetano@users.noreply.github.com>
2024-11-09 17:50:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ window.top === window && !function(){
     e.async = !0;
     t.appendChild(e);
 }();
+function setup() {
+    console.log("Setup iniciado");
+    noCanvas();
+    setTimeout(() => {
+        waterLevel -= 1; // Perda inicial de 1ml após 15 segundos
+        updateWaterDisplay();
+        intervalId = setInterval(updateWaterLevel, 35000); // Intervalo de 35 segundos
+    }, 15000); // Atraso de 15 segundos
+    setInterval(updateClock, 1000);
+
+    document.getElementById('pause-button').addEventListener('click', togglePause);
+    document.getElementById('reset-button').addEventListener('click', resetSystem);
+    document.getElementById('print-button').addEventListener('click', imprimirResultados);
+}
