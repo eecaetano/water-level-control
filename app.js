9954ea0fdbe957109e79cc68da7b615e97a6a62c
ANDERSON R OLIVEIRA <103950004+eecaetano@users.noreply.github.com>
2024-11-09 23:58:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let waterLevel = 20000; // Nível inicial da água em ml
 const maxWaterLevel = 20000; // Nível máximo da água em ml
-const minWaterLevel = 49; // Nível mínimo da água em ml
+const minWaterLevel = 51; // Nível mínimo da água em ml
 let intervalId;
 let isPaused = false;
 
@@ -88,18 +88,18 @@ function resetSystem() {
     document.getElementById('log-table').getElementsByTagName('tbody')[0].innerHTML = '';
     updateWaterDisplay();
     setTimeout(() => {
-        waterLevel -= 1; // Perda inicial de 1ml após 15 segundos
+        waterLevel -= 1; // Perda inicial de 1000ml após 15 segundos
         updateWaterDisplay();
-        intervalId = setInterval(updateWaterLevel, 35000); // Intervalo de 35 segundos
-    }, 15000); // Atraso de 15 segundos
+        intervalId = setInterval(updateWaterLevel, 25000); // Intervalo de 25 segundos
+    }, 10000); // Atraso de 10 segundos
 }
 
 function imprimirResultados() {
     const capacidadeReservatorio = 20000; // Capacidade do reservatório em ml
     const resultados = [
-        { dataHora: "2024-10-12 10:00", alerta: "Nível de água crítico", volume: 15000 },
-        { dataHora: "2024-10-12 11:00", alerta: "Nível de água crítico", volume: 14000 },
-        { dataHora: "2024-10-12 12:00", alerta: "Nível de água crítico", volume: 13000 }
+        { dataHora: "2024-10-12 10:00", alerta: "Queda do nível de água", volume: 15000 },
+        { dataHora: "2024-10-12 11:00", alerta: "Reservatório com 50% da capacidade, volume: 10000 },
+        { dataHora: "2024-10-12 12:00", alerta: "Nível de água crítico", volume: 5000 }
     ];
 
     resultados.forEach(resultado => {
